96384abf04fb2467c66529be35b45dbe398b1308
Aji Soriano <ajisoriano02@gmail.com>
2020-11-20 22:51:44 +0800

    Edited QR Generator and Scanner

--- app/src/main/java/com/example/dfect/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/dfect/ui/notifications/NotificationsFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.dfect.R
 
-class NotificationsFragment : Fragment() {
+
+class NotificationsFragment : Fragment()  {
+
+
 
     private lateinit var notificationsViewModel: NotificationsViewModel
 
@@ -19,13 +22,19 @@ class NotificationsFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        notificationsViewModel =  ViewModelProvider(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val ListView = root.findViewById<ListView>(R.id.scannedListView)
+        val values = arrayOf("Scanned 1", "Scanned 2", "Scanned 3", "Scanned 4", "Scanned 5",
+                "Scanned 6", "Scanned 7", "Scanned 8", "Scanned 9", "Scanned 10"
+                , "Scanned 11", "Scanned 12", "Scanned 13", "Scanned 14", "Scanned 15")
+        val adapter = activity?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, values) }
+        ListView.adapter = adapter
+//        val textView: TextView = root.findViewById(R.id.text_notifications)
+//
+//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
